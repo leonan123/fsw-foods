b0c57f637f5b5075971b2b5f2c580d915df3ed5e
leonan123 <leonanorth3@gmail.com>
2024-04-30 01:46:32 -0300

    feat: add promo banner to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { CategoryList } from './_components/category-list'
 import { Header } from './_components/header'
 import { Search } from './_components/search'
@@ -13,6 +15,18 @@ export default function Home() {
 
       <div className="mt-6 px-5">
         <CategoryList />
+      </div>
+
+      <div className="mt-6 px-5">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          sizes="100vw"
+          quality={100}
+          className="h-auto w-screen"
+        />
       </div>
     </div>
   )
