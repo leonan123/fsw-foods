45a0d1ec18a599e9ea5bf86601dc14e135d52168
leonan123 <leonanorth3@gmail.com>
2024-04-30 00:04:02 -0300

    feat: add header

---
+++ src/app/_components/ui/header.tsx
@@ -0,0 +1,19 @@
+import { MenuIcon } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Button } from './button'
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-between px-5 pt-6">
+      <Link href="#">
+        <Image src="/logo.svg" alt="FSW Foods" width={100} height={30} />
+      </Link>
+
+      <Button variant="ghost" size="icon">
+        <MenuIcon />
+      </Button>
+    </header>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Header } from './_components/ui/header'
+
 export default function Home() {
   return (
     <div>
-      <h1 className="mt-2 box-border flex items-center bg-red-500 py-2"></h1>
+      <Header />
     </div>
   )
 }
